6b84c13446935aaff5f005b51b1b862e09021ff0
Juan <dngeldark@gmail.com>
2020-06-30 17:41:13 -0500

    submit ready

--- source.js
+++ source.js
@@ -2,17 +2,18 @@ let editBtn = document.querySelector('.profile__edit-btn');
 let closeBtn = document.querySelector('.popup__close-btn');
 let saveBtn = document.querySelector('.form__save-btn');
 
-function toggleModal() {
+function toggleModal(e) {
   document.querySelector('.popup').classList.toggle('popup_opened');
+  e.preventDefault();
 }
 
-function editInfo(evt) {
+function editInfo(e) {
   let profileName = document.querySelector('.profile__name');
   let profileJob = document.querySelector('.profile__job');
   let job = document.querySelector('.form__profession');
   let name = document.querySelector('.form__name');
 
-  evt.preventDefault();
+  e.preventDefault();
   if (name.value.length > 0) {
     profileName.innerText = name.value;
     profileJob.innerText = job.value;
@@ -20,7 +21,7 @@ function editInfo(evt) {
     job.value = '';
   }
 
-  toggleModal();
+  toggleModal(e);
 }
 
 closeBtn.addEventListener('click', toggleModal);
