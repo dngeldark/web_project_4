c47f5634c77dfc7b618aa01f3735dd94b6797af0
Juan <dngeldark@gmail.com>
2020-07-03 20:56:50 -0500

    add features

--- source.js
+++ source.js
@@ -1,3 +1,42 @@
+const initialCards = [
+  {
+    name: 'The Wave',
+    liked: false,
+    link:
+      'https://images.unsplash.com/photo-1511206221321-715b8bae1613?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  },
+
+  {
+    name: 'Yosemite Valley',
+    liked: false,
+    link: 'https://code.s3.yandex.net/web-code/yosemite.jpg',
+  },
+  {
+    name: 'Oklahoma',
+    liked: false,
+    link:
+      'https://images.unsplash.com/photo-1593227948434-19f6f6eba234?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+  },
+  {
+    name: 'Lake Louise',
+    liked: false,
+    link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+  },
+
+  {
+    name: 'Smokie Mountains',
+    liked: false,
+    link:
+      'https://images.unsplash.com/photo-1536691661814-8b0372158cf5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+  },
+  {
+    name: 'Olympic NP',
+    liked: false,
+    link:
+      'https://images.unsplash.com/photo-1473442240418-452f03b7ae40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMjF9&auto=format&fit=crop&w=1050&q=80',
+  },
+];
+
 const editBtn = document.querySelector('.profile__edit-btn');
 const closeBtn = document.querySelector('.popup__close-btn');
 const formElement = document.querySelector('.form');
@@ -5,6 +44,12 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const jobInput = document.querySelector('.form__profession');
 const nameInput = document.querySelector('.form__name');
+const formTitle = document.querySelector('.form__title');
+const modal = document.querySelector('.picture-modal');
+const modalPicture = document.querySelector('.picture-modal__image');
+const modalTitle = document.querySelector('.picture-modal__title');
+const addButton = document.querySelector('.profile__add-btn');
+const cards = document.querySelector('.cards');
 
 function toggleModal() {
   document.querySelector('.popup').classList.toggle('popup_opened');
@@ -14,9 +59,93 @@ function formSubmitHandler(e) {
   e.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  formElement.reset();
   toggleModal();
 }
 
 closeBtn.addEventListener('click', toggleModal);
-editBtn.addEventListener('click', toggleModal);
-formElement.addEventListener('submit', formSubmitHandler);
+
+editBtn.addEventListener('click', () => {
+  toggleModal();
+  jobInput.placeholder = 'Profession';
+  nameInput.placeholder = 'Name';
+  formTitle.textContent = 'Edit Profile';
+  formElement.removeEventListener('submit', addBtnSubmitHandler);
+  formElement.addEventListener('submit', formSubmitHandler);
+});
+
+//create Card element
+const createCard = function (item) {
+  const div = document.createElement('div');
+  div.append(document.querySelector('#card-template').content.cloneNode(true));
+  const cardPicture = div.querySelector('.card__picture');
+  const likeBtn = div.querySelector('.card__like-btn');
+  cardPicture.src = item.link;
+  cardPicture.alt = item.name;
+  div.querySelector('.card__title').textContent = item.name;
+
+  //like-button click handle
+  likeBtn.addEventListener('click', () => {
+    item.liked = !item.liked;
+    const test = document.createElement('div');
+    test.textContent = 'test';
+  });
+
+  //delete-button click handle
+  div.querySelector('.card__delete-btn').addEventListener('click', (e) => {
+    initialCards.splice(initialCards.indexOf(item), 1);
+    div.remove();
+  });
+
+  //click handle on card__picture , picture-modal opens
+  cardPicture.addEventListener('click', (e) => {
+    modalPicture.src = e.target.src;
+    modalTitle.textContent = e.target.alt;
+    modal.classList.toggle('fade-out');
+
+    //modal closes when the picture in it is clicked
+    modalPicture.addEventListener('click', (evt) => {
+      modal.classList.toggle('fade-out');
+      evt.stopImmediatePropagation();
+    });
+  });
+  return div;
+};
+
+//create the cards and append to the cards container
+const renderCards = function () {
+  //const cards = document.querySelector('.cards');
+  //const cardArr = cards.children;
+  //clear cards each time a new one is added
+  //Array.from(cardArr).forEach((element) => {
+  //  element.remove();
+  //});
+
+  initialCards.map((item) => {
+    cards.append(createCard(item));
+  });
+};
+
+//handle submit for New Place form
+function addBtnSubmitHandler(evt) {
+  evt.preventDefault();
+  evt.stopImmediatePropagation();
+  const newCard = { name: nameInput.value, link: jobInput.value, liked: false };
+  initialCards.push(newCard);
+  formElement.reset();
+  cards.append(createCard(newCard));
+  console.log(cards);
+  toggleModal();
+}
+
+//add event listener to add button
+addButton.addEventListener('click', () => {
+  toggleModal();
+  nameInput.placeholder = 'Title';
+  jobInput.placeholder = 'Image URL';
+  formTitle.textContent = 'New Place';
+  formElement.removeEventListener('submit', formSubmitHandler);
+  formElement.addEventListener('submit', addBtnSubmitHandler);
+});
+
+renderCards();
